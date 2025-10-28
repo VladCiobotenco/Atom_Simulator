e5b91274d58540417e4fa7ad0306c854f5bc8c31
Vlad Ciobotenco <73481862+VladCiobotenco@users.noreply.github.com>
2025-10-28 13:05:32 +0200

    Modified run_cppcheck.sh to ignore 'libraries' v3

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -3,8 +3,8 @@
 cppcheck --enable=all \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
-    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
-    -i"${EXT_DIR:-libraries}" --suppress="*:${EXT_DIR:-libraries}/*" \
+    --suppress="*:*${BUILD_DIR:-build}/*" \
+    --suppress="*:*${EXT_DIR:-libraries}/*" \
     --suppress=missingIncludeSystem \
     --suppress=unmatchedSuppression \
     --suppress=useStlAlgorithm \
